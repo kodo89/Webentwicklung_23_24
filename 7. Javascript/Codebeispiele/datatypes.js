23b827b8dc8fa7370dfaecb7991853a2711b34d9
Holdo89 <d.holzweber@hotmail.com>
2023-10-24 15:29:16 +0200

    Unterlagen zu Tag 7

---
+++ 7. Javascript/Codebeispiele/datatypes.js
@@ -0,0 +1,31 @@
+let myNumber = 42;
+
+let myString = "Hallo, Welt!";
+
+let myBoolean = true;
+
+let myArray = [1, 2, 3, 4, 5];
+
+let myObject = {
+    name: "Max",
+    age: 30
+};
+
+let nullVar = null
+
+let undefinedVar
+
+console.log(typeof myNumber);
+console.log(typeof myString);
+console.log(typeof myBoolean);
+console.log(typeof myArray);
+console.log(typeof myObject);
+console.log(typeof nullVar)
+console.log(typeof undefinedVar)
+
+
+let myYodaString = "Coden Ihr wollt lernen junge CodersBay Schüler";
+
+console.log(myYodaString.indexOf("Code")) //  0
+console.log(myYodaString.lastIndexOf("Code")) // 29
+
