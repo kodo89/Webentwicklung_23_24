e2dcb8f8a51428a7a3c930fefbd7283f36d5c23a
WOLFGANG SCHUEBL <wolfus.sch@gmail.com>
2023-10-24 21:34:42 +0200

    Java

--- 0.Eure Codebeispiele/Wolfgang/JavaScript/variables.js
+++ 0.Eure Codebeispiele/Wolfgang/JavaScript/variables.js
@@ -5,7 +5,9 @@ let b = "8";
 /***********Ändere nicht den Code darüber 👆*******/
 // Schreibe Code mit dem die Werte für a und b vertauscht werden
 // Eingabe von Zahlen oder Strings ist nicht erlaubt
-
+let tausch = a;
+a = b;
+b = tausch;
 
 /***********Ändere nicht den Code darunter👇*******/
 
