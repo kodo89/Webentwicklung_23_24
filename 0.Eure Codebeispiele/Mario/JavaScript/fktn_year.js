98382321839783c6fb6ead5b3540493de16fd4aa
Mario Brandl <brandl.mario@gmx.at>
2023-11-04 12:41:32 +0100

    leapYearV2

--- 0.Eure Codebeispiele/Mario/JavaScript/fktn_year.js
+++ 0.Eure Codebeispiele/Mario/JavaScript/fktn_year.js
@@ -4,9 +4,14 @@
 
 
 
-const year = 2400;
+const year = 2104;
 const printLeapYear = leapYear(year);
 console.log(printLeapYear);
+
+
+const printLeapYear2 = leapYearV2(year);
+console.log(printLeapYear2);
+
 
 
 
@@ -17,6 +22,20 @@ function leapYear(y){
         return false;
     } else if(y % 4 == 0){
         return true;
+    } else{
+        return false;
+    }
+}
+
+function leapYearV2(y){
+    if(y % 4 == 0){
+        if(y % 100 == 0 && y % 400 == 0){
+            return true;
+        } else if (y % 100 == 0){
+            return false;
+        } else{
+            return true;
+        }
     } else{
         return false;
     }
