d02a193d542afd8b012e24564d1f27271420eae2
Holdo89 <d.holzweber@hotmail.com>
2023-11-04 08:10:26 +0100

    Unterlagen Tag 7

---
+++ 7. Javascript/Codebeispiele/switchcase.js
@@ -0,0 +1,16 @@
+let fruit = 'Banane';
+
+switch(fruit) {
+  case 'Apfel':
+    console.log('Äpfel sind rot.');
+    break;
+  case 'Banane':
+    console.log('Bananen sind gelb.');
+    break;
+  case 'Orange':
+    console.log('Orangen sind orange.');
+    break;
+  default:
+    console.log('Ich kenne diese Frucht nicht.');
+    break;
+}
