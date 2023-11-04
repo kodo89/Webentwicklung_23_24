e96b23ce2cea128f28db77b7b0b368fcc3ddcef3
Matthias Langwieser <37292244+lamacoding@users.noreply.github.com>
2023-11-04 10:14:19 +0100

    Solve if else exercise

--- 0.Eure Codebeispiele/Matthias/JavaScript/ifelse.js
+++ 0.Eure Codebeispiele/Matthias/JavaScript/ifelse.js
@@ -1,12 +1,44 @@
 //Lies vom Nutzer eine Temperatur ein, nutze ein if/else-if/else um auszugeben, ob es kalt, angenehm oder heiß ist.
+let temperature = prompt("Temperature: ");
 
+if(temperature < 15) {
+    alert("Brrr, it's cold!");
+} else if(temperature < 28) {
+    alert("Nice temperature.");
+} else {
+    alert("Quite hot in here!")
+}
 
 //Lies einen einzelnen Buchstaben ein und gib aus, ob es ein Vokal oder Konsonant ist. Nutze dafür ein switch-case mit default case und fall-through.
+let letter = prompt("Letter: ");
 
+switch(letter.toLocaleLowerCase()) {
+    case 'a':
+    case 'e':
+    case 'i':
+    case 'o':
+    case 'u':
+        alert("Vowel");
+        break;
+    default:
+        alert("Consonant");
+        break;
+}
 
 //Lese 3 Zahlen vom Benutzer ein, und gib die größte davon zurück. Nutze dabei Ternäre Ausdrücke. 
+let num1 = prompt("Number 1:");
+let num2 = prompt("Number 2:");
+let num3 = prompt("Number 3:");
 
+alert("Maximum: " + num1 > num2 ? (num1 > num3 ? num1 : num3) : (num2 > num3 ? num2 : num3));
   
 //Lies eine Zahl vom Benutzer ein, und gebe aus, ob diese gerade, ungerade oder eine Kommazahl ist.
+let number = prompt("Number: ");
 
-
+if(number.indexOf('.') >= 0) {
+    alert("Comma number");
+} else if(number % 2 == 0) {
+    alert("Even number");
+} else {
+    alert("Odd number");
+}
