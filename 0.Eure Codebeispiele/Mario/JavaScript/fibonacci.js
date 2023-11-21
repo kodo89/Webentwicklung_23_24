e299154789217d7c722386b189d85acdbe4cad8d
Mario Brandl <brandl.mario@gmx.at>
2023-11-21 18:38:23 +0100

    commitFibonaci

--- 0.Eure Codebeispiele/Mario/JavaScript/fibonacci.js
+++ 0.Eure Codebeispiele/Mario/JavaScript/fibonacci.js
@@ -2,38 +2,36 @@
 // Beispiel: generateFibonacci(10)
 // Output: 0,1,1,2,3,5,8,13,21,34
 
-
-
-
-let x = 10;
-
-
-
-
-let i = 0;
-
-const arr = new Array(x);
-
-let output = " ";
-
-while(i < x){
+function generateFibonacci(x){
+    let i = 0;
+    const arr = new Array(x);
+    let output = " ";
     
-    if(i < 2){
-        arr[i] = i;
-
-    } else{
-        arr[i] = arr[i-1]+arr[i-2];
+    while(i < x){
+    
+        if(i < 2){
+            arr[i] = i;
+    
+        } else{
+            arr[i] = arr[i-1]+arr[i-2];
+        }
+    
+        if(i>x-2){
+            output = output + arr[i]
+        } else{
+            output = output + arr[i] + ", "
+        }
+    
+        i++;
     }
-
-    if(i>x-2){
-        output = output + arr[i]
-    } else{
-        output = output + arr[i] + ", "
-    }
-
-    i++;
+    return output;
 }
-console.log(output);
+
+console.log(generateFibonacci(10));
+
+
+
+
 
 
 
