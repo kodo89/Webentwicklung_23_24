aab08e2b8d53b89e1d61e181224bab1c5daa4132
Matthias Langwieser <37292244+lamacoding@users.noreply.github.com>
2023-11-28 18:49:40 +0100

    Solve divide error exercise

--- 0.Eure Codebeispiele/Matthias/JavaScript/divideError.js
+++ 0.Eure Codebeispiele/Matthias/JavaScript/divideError.js
@@ -1,23 +1,23 @@
-<<<<<<< HEAD
 // Schreibe eine Funktion die 2 Zahlen als Parameter übergibt und beide Zahlen miteinander dividiert (erste / zweite)
 // Füge eine Fehlerbehandlung ein für den Fall dass:
 // Anstelle von Zahlen, Strings übergeben wurden
 // Die 2. Zahl die übergeben wurde = 0 (Division durch 0)
 
+function divideNumbers(firstNumber, secondNumber) {
+  try {
+    if (isNaN(firstNumber) || isNaN(secondNumber)) {
+      throw new Error("One of the parameters is not a number.");
+    } else if (secondNumber === 0) {
+      throw new Error("Division by 0 is not possible.");
+    } else {
+      console.log(firstNumber / secondNumber);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 // Beispielaufrufe
-divideNumbers(10, 2);   // Gültige Division
-divideNumbers(5, 0);    // Ungültige Division, sollte einen Fehler auslösen
-divideNumbers(8, 4);    // Gültige Division
-=======
-// Schreibe eine Funktion die 2 Zahlen als Parameter übergibt und beide Zahlen miteinander dividiert (erste / zweite)
-// Füge eine Fehlerbehandlung ein für den Fall dass:
-// Anstelle von Zahlen, Strings übergeben wurden
-// Die 2. Zahl die übergeben wurde = 0 (Division durch 0)
-
-
-// Beispielaufrufe
-divideNumbers(10, 2);   // Gültige Division
-divideNumbers(5, 0);    // Ungültige Division, sollte einen Fehler auslösen
-divideNumbers(8, 4);    // Gültige Division
->>>>>>> 5035c211a284295ab1ae80c6f48d55efd43f6298
+divideNumbers(10, 2); // Gültige Division
+divideNumbers(5, 0); // Ungültige Division, sollte einen Fehler auslösen
+divideNumbers(8, 4); // Gültige Division
