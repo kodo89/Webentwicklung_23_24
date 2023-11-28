e63f88632147dd31b67c521a5324a5121e66bff6
Mario Brandl <brandl.mario@gmx.at>
2023-11-28 19:57:57 +0100

    commitBlackJack2

--- 0.Eure Codebeispiele/Mario/JavaScript/BlackJack/index.js
+++ 0.Eure Codebeispiele/Mario/JavaScript/BlackJack/index.js
@@ -1,14 +1,39 @@
 
-
-
-
-
 /* 1000 Euro Startkapital */
 let valueStartingKapital = 1000;
 let valueInput = 5;
 
-const arrayCards = new Array(4).fill(new Array(13));
 
+
+const arr = ["AssHerz", "AssPik", "AssKaro", "AssEichel", ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+const arrayCards = new Array(4).fill(new Array(13));
+*/
 /*
 const m = 4;
 const  n = 13;
@@ -22,6 +47,10 @@ let arrayCards = Array(m).fill().map(() => Array(n));*/
 
 
 /* Zahlenwerte Karten */
+
+
+
+/*
 for(let i = 0; i < arrayCards.length; i++){
     for(let j = 0; i < arrayCards[i].length; j++){
 
@@ -32,7 +61,7 @@ for(let i = 0; i < arrayCards.length; i++){
         } else{
             arrayCards[i][j] = 10;
         }
-    } 
+    }
 }
 
 
@@ -44,7 +73,7 @@ for(let i = 0; i < arrayCards.length; i++){
     } 
 }
 
-
+*/
 
 
 
