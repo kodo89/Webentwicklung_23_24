88ace50e5737f31c477700b7bd303f81f3e3cd1e
Holdo89 <d.holzweber@hotmail.com>
2023-11-28 19:09:46 +0100

    Lösung zu Drumkit

--- 7. Javascript/Codebeispiele/divideError.js
+++ 7. Javascript/Codebeispiele/divideError.js
@@ -4,14 +4,18 @@ function divideNumbers(a, b) {
         if (b === 0) {
             throw new Error('Division durch Null ist nicht erlaubt.');
         }
+        if(typeof(a)!=="number"||typeof(b)!=="number")
+        {
+            throw new Error("Mindestens einer der beiden Parameter ist keine Zahl")
+        }
         var result = a / b;
         console.log('Ergebnis der Division:', result);
     } catch (error) {
         console.error('Fehler aufgetreten:', error.message);
     }
+    console.log("Hallo")
 }
 
 // Beispielaufrufe
-divideNumbers(10, 2);   // Gültige Division
-divideNumbers(5, 0);    // Ungültige Division, sollte einen Fehler auslösen
-divideNumbers(8, 4);    // Gültige Division
+  // Ungültige Division, sollte einen Fehler auslösen
+divideNumbers([1,2], 2);    // Gültige Division
