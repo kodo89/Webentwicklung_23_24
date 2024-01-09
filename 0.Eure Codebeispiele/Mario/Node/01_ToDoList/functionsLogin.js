2a0961f8026a91b0331f724f12a379aa88ec0a72
Mario Brandl <brandl.mario@gmx.at>
2024-01-09 17:08:59 +0100

    todo

---
+++ 0.Eure Codebeispiele/Mario/Node/01_ToDoList/functionsLogin.js
@@ -0,0 +1,90 @@
+//Login Functions
+document.getElementById("loginButton").addEventListener("click", userOrPasswordIsFalse);
+
+function userOrPasswordIsFalse(){
+    if(testingIfAnyUserIsRegistrated()){
+        window.alert("Keine Nutzer registriert - Bitte registrieren!")
+    } else if(inputUserNameEmpty() || inputUserPWEmpty()){
+        window.alert('Bitte die Eingabefelder "Benutzer" und "Passwort" ausfüllen!');
+    } else if (getIndexFromUserObject() == -1){
+        window.alert("Benutzer ist falsch!");
+    } else if (getUserPasswordInput() != getObjectUserFromStorage().password[getIndexFromUserObject()]){
+        window.alert("Passwort ist falsch!")
+    } else{
+        localStorage.setItem('activeUserIndex', getActiveUserID());
+        window.alert("Login erfolgreich - Sie werden zur ToDo Liste weitergeleitet!")
+        window.location.href = 'http://127.0.0.1:5500/03_Projects/01_ToDoList/todoList.html';
+    }
+}
+
+
+function testingIfAnyUserIsRegistrated(){
+    if(localStorage.getItem('objectUser') == null){
+        return true;
+    } else if(localStorage.getItem('objectUser') != null){
+        var object = JSON.parse(localStorage.getItem('objectUser'));
+        if(object.user.length == 0){
+            return true;
+        }
+    }
+}
+
+
+function inputUserNameEmpty(){
+    if(document.getElementById("userName").value == ""){
+        return true;
+    } else{
+        return false;
+    }
+}
+
+
+function inputUserPWEmpty(){
+    if(document.getElementById("password").value == ""){
+        return true;
+    } else{
+        return false;
+    }
+}
+
+
+function getObjectUserFromStorage(){
+    return JSON.parse(localStorage.getItem('objectUser'));
+}
+
+
+function getIndexFromUserObject(){
+    return getObjectUserFromStorage().user.indexOf(getUserNameInput());
+}
+
+function getActiveUserID(){
+    return getObjectUserFromStorage().userID[getIndexFromUserObject()];
+}
+
+
+
+// get User Inputs
+
+function getUserNameInput(){
+    return document.getElementById("userName").value;
+}
+
+function getUserPasswordInput(){
+    return document.getElementById("password").value;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
