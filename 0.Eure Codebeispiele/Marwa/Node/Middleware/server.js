358d4ea719d7eb5b7a741a2fe1a51ed5b87a7a4d
Marwa Abdelgawad <abdelgawad.m@outlook.de>
2024-01-23 18:04:11 +0100

    v

--- 0.Eure Codebeispiele/Marwa/Node/Middleware/server.js
+++ 0.Eure Codebeispiele/Marwa/Node/Middleware/server.js
@@ -1,12 +1,18 @@
 //Hier kommt dein Webserver der das index.html File zurückliefert
 
 import express from "express";
+import {dirname} from "path";
+import {fileURLToPath} from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const app = express();
 const port = 3000;
 
+
 app.get("/", (req, res) => {
-    res.send("<h1>Home Page</h1>");
-  });
+  res.sendFile(__dirname + "/public/index.html");
+});
 
 
 app.listen(port, () => {
