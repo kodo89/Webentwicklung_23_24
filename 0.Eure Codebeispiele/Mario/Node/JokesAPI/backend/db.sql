ef733bfb6587e485754a87b06bbd5a151c081416
Mario Brandl <brandl.mario@gmx.at>
2024-01-30 21:46:38 +0100

    db

---
+++ 0.Eure Codebeispiele/Mario/Node/JokesAPI/backend/db.sql
@@ -0,0 +1,28 @@
+
+
+
+
+Create table jokes
+(
+    id integer,
+    jokeText Varchar(100),
+    jokeType Varchar(30),
+
+    Primary Key (id)
+);
+
+
+
+INSERT
+INTO jokes (id, jokeText, jokeType)
+VALUES 
+(1, "Why don't scientists trust atoms? Because they make up everything.", "Science"),
+(2, "Why did the scarecrow win an award? Because he was outstanding in his field.", "Puns"),
+(3, "I told my wife she was drawing her eyebrows too high. She looked surprised.", "Puns"),
+(4, "What did one ocean say to the other ocean? Nothing, they just waved.", "Wordplay"),
+(5, "Why do we never tell secrets on a farm? Because the potatoes have eyes and the corn has ears.", "Wordplay");
+
+
+
+
+
