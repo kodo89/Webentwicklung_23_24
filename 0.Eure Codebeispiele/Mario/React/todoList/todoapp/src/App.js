4465dfaa2e4ca88944a18535d4cfd6d7005e1bea
Mario Brandl <brandl.mario@gmx.at>
2024-03-19 20:07:43 +0100

    materialui

---
+++ 0.Eure Codebeispiele/Mario/React/todoList/todoapp/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Materialui from './components/Test';
+
+function App() {
+  return (
+    <>
+        <Materialui/>
+    </>
+  );
+}
+
+export default App;
