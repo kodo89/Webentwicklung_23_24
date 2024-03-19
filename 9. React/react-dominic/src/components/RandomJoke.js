14262ccc8d4bce39ca04c02a18142baa81a31c7f
Holdo89 <d.holzweber@hotmail.com>
2024-03-19 17:05:07 +0100

    React Finale Folien

--- 9. React/react-dominic/src/components/RandomJoke.js
+++ 9. React/react-dominic/src/components/RandomJoke.js
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from "react";
 export default function RandomJoke() {
   const [joke, setJoke] = useState("")
   function myFetchFunction() {
-    fetch("http://localhost:3000/random")
+    fetch("/random")
       .then((response) => response.json())
       .then((data) => setJoke(data.jokeText));
   }
