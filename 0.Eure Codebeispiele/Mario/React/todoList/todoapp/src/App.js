e8e898cbf3c639e459dda8b700143e702fc5256d
Mario Brandl <brandl.mario@gmx.at>
2024-03-19 20:27:09 +0100

    totoreact

--- 0.Eure Codebeispiele/Mario/React/todoList/todoapp/src/App.js
+++ 0.Eure Codebeispiele/Mario/React/todoList/todoapp/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import Materialui from './components/Test';
+import Materialui from './components/Myfirstbutton';
+import Myfirstinput from './components/Myfirstinputfield';
 
 function App() {
   return (
     <>
         <Materialui/>
+        <Myfirstinput/>
     </>
   );
 }
