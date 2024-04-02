5c7246d375d2b06914af45b01e108898814b40f7
WOLFGANG SCHUEBL <wolfus.sch@gmail.com>
2024-04-02 18:04:30 +0200

    02.04

--- 0.Eure Codebeispiele/Wolfgang/React/mat-uebung/src/App.js
+++ 0.Eure Codebeispiele/Wolfgang/React/mat-uebung/src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+
+import "./App.css";
+import BasicButtons from "./Button.js";
+import BasicStack from "./Stack.js";
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <BasicStack spacing={2}>
+        </BasicStack>
+
+        <BasicButtons variant="text">Text</BasicButtons>
       </header>
     </div>
   );
