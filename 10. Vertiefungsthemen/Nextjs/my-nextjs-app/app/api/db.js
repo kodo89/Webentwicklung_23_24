7ea2c7b5ca2108d0047a1ef0981cac3ce59a8cc5
Holdo89 <d.holzweber@hotmail.com>
2024-07-03 07:33:46 +0200

    Nextjs Material

---
+++ 10. Vertiefungsthemen/Nextjs/my-nextjs-app/app/api/db.js
@@ -0,0 +1,23 @@
+import mysql from 'mysql';
+
+const pool = mysql.createPool({
+  connectionLimit: 10,
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "feedcube_codersbay",
+});
+
+export function query(sql, values) {
+  return new Promise((resolve, reject) => {
+    pool.query(sql, values, (error, results) => {
+      if (error) {
+        return reject(error);
+      }
+      resolve(results);
+    });
+  });
+}
+
+
+
---
+++ 10. Vertiefungsthemen/Nextjs/my-nextjs-app/app/users/edit/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function editUser() {
+  return (
+    <div>
+      Hier wird ein User bearbeitet
+    </div>
+  )
+}
